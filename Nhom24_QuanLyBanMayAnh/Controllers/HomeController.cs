7db3ac042e845ad933f667a707a27eb74b094dcc
ADMIN <lethikimngan.dn43@gmail.com>
2024-11-26 22:19:31 +0700

    Add project files.

---
+++ Nhom24_QuanLyBanMayAnh/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Nhom24_QuanLyBanMayAnh.Models;
+using System.Diagnostics;
+
+namespace Nhom24_QuanLyBanMayAnh.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Nhom24_QuanLyBanMayAnh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nhom24_QuanLyBanMayAnh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
